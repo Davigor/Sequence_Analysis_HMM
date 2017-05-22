0540ee3e4ae434f273a1b4dd28b63b6775d87ac8
David McGee <david.m.mcgee1@gmail.com>
2017-05-22 19:16:07 -0400

    Refactored; broken down into functions

--- C_implementation/durbin_example/viterbi_durbin.c
+++ C_implementation/durbin_example/viterbi_durbin.c
@@ -3,105 +3,124 @@
  * Program automatically determines n value from sequence file and assumes that state file has same n value.
  * 
  * Program follows example from Durbin et. al.'s "The occasionally dishonest casino, part 1." on p. 54 of Biological Sequence
- * Analyis (2002), with solution and viterbi output given on p. 57. The two states, F and L, correspond to a "Fair" or a "Loaded" die.
+ * Analyis, with solution and viterbi output given on p. 57. The two states, F and L, correspond to a "Fair" or a "Loaded" die.
  * 
- * free .pdf of Durbin at: http://dnapunctuation.org/~poptsova/course/Durbin-Et-Al-Biological-Sequence-Analysis-CUP-2002-No-OCR.pdf
+ * free .pdf of Durbin at: http://dnapunctuation.org/~poptsova/course/Durbin-Et-Al-Biological-Sequence-Analysis-CUP-2002-No-OCR.pdf 
  * 
  * Optional argument to read in file of known states for comparison with algorithm's output. 
  * Sequence file and state files are is assumed to be one entry per line (see .txt files for example).
  * 
- * Usage: ./viterbi my_sequence_file.txt my_state_file.txt
- * my_sequence_file.txt = sequence file (required)
- * my_state_file.txt = state file (optional)
+ * Usage: ./viterbi my_sequence_file.txt [my_state_file.txt]
  *
 **/
-
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <ctype.h>
+#include <err.h>
+#include <sysexits.h>
 
-double max (double a, double b);
-int argmax (double row0, double row1);
+int sequence_length = 0;
+int* sequence;
+int* states;
 
-int main (int argc, char *argv[]) 
+static double max (double a, double b)
 {
-    // check for correct number of command line args
-    if (argc != 2 && argc != 3)
+    if (a > b)
     {
-        printf("Usage: ./viterbi my_sequence_file.txt my_state_file.txt .  Include at least sequence file.\n");
+        return a;
+    }
+    // if equal, returns arbitrary argument for specific use in this algorithm
+    else
+    {
+        return b;
+    }
+}
+
+static int argmax (double row0, double row1)
+{
+    if (row0 > row1)
+    {
+        return 0;
+    }
+    else
+    {
         return 1;
     }
-    
-    // open sequence file and store in array. Dynamically allocate memory and automatically detect sequence n value
-    FILE *seqf = fopen(argv[1], "r");
+}
+
+static int* read_sequencefile(const char* sequence_file, int* out_n)
+{
+    FILE *seqf = fopen(sequence_file, "r");
     if (!seqf)
     {
-        printf("Invalid sequence file.\n");
-        return 1;
+        fprintf(stderr, "Invalid sequence file.\n");
+        return NULL;
     }
     
     int num;
-    int memsize = 100;
     int n = 0;
-    int *seq = calloc(memsize, sizeof(int));
+    sequence = malloc(sizeof(int));
     
     while(fscanf(seqf, "%i", &num) == 1)
     {
-        seq[n] = num - 1;
+        sequence[n] = num - 1;
         n++;
-
-        if (n == memsize)
-        {
-            memsize += 100;
-            seq = realloc(seq, memsize * sizeof(int));
-        }
+        sequence = realloc(sequence, (n + 1) * sizeof(int));
         
-        if(!seq)
+        if(!sequence)
         {
-            printf("Not enough memory.");
-            return 1;
+            fprintf(stderr, "Not enough memory.");
+            return NULL;
         }
     }
     fclose(seqf);
     
-    // if passed as an argument, open the state solution file and print. Assumes n is same as sequence n above
-    if(argv[2])
+    *out_n = n;
+    return sequence;
+}
+
+
+static int read_statefile(const char* state_file)
+{
+    FILE *statef = fopen(state_file, "r");
+    
+    if (!statef)
     {
-        FILE *statef = fopen(argv[2], "r");
-        
-        if (!statef)
-        {
-            printf("Invalid state file.\n");
-            return 1;
-        }
-        
-        char *state = calloc(n, sizeof(char));
-        if(!state)
-        {
-            printf("Not enough memory.");
-            return 1;
-        }
-        
-        char ch;
-        printf("State solution:\n");
-        for (int i = 0; i < n; i++)
-        {
-            fscanf(statef, "%c %*[\r\n]", &ch);
-            state[i] = ch;
-            
-            if (i % 60 == 0 && i != 0)
-            {
-                printf("\n");
-            }
-            printf("%c", state[i]);
-        }
-        fclose(statef);
-        free(state);
-        printf("\n\n");
+        fprintf(stderr, "Invalid state file.\n");
+        return EXIT_FAILURE;
     }
     
+    char *state = malloc(sequence_length * sizeof(char));
+    if(!state)
+    {
+        fprintf(stderr, "Not enough memory.");
+        return EXIT_FAILURE;
+    }
+    
+    char ch;
+    printf("State solution:\n");
+    for (int i = 0; i < sequence_length; i++)
+    {
+        fscanf(statef, "%c %*[\r\n]", &ch);
+        state[i] = ch;
+        
+        if (i % 60 == 0 && i != 0)
+        {
+            printf("\n");
+        }
+        printf("%c", state[i]);
+    }
+    fclose(statef);
+    free(state);
+    printf("\n\n");
+    return 0;
+}
+
+
+static int run_viterbi(int* seq, int seq_length)
+{
     // state transition matrix in log space
     double a[2][2] = {
         { log(0.95),  log(0.05) },
@@ -119,26 +138,26 @@ int main (int argc, char *argv[])
     };
     
     // allocate rest of memory and error handle
-    int *path = calloc(n, sizeof(double));
-    double **vprob = calloc(n, sizeof(double *));
-    double **ptr = calloc(n, sizeof(double *));
-    double **pi = calloc(n, sizeof(double *));
+    int *path = malloc(seq_length * sizeof(path));
+    double **vprob = malloc(seq_length * sizeof(*vprob));
+    double **ptr = malloc(seq_length * sizeof(*ptr));
+    double **pi = malloc(seq_length * sizeof(*pi));
     
     if( !path || !vprob || !ptr || !pi )
         {
-            printf("Not enough memory.");
+            fprintf(stderr, "Not enough memory.");
             return 1;
         }
     
     for (int i = 0; i < 2; i++)
     {
-        vprob[i] = calloc(n, sizeof(double));
-        ptr[i] = calloc(n, sizeof(double));
-        pi[i] = calloc(n, sizeof(double));
+        vprob[i] = malloc(seq_length * sizeof(double));
+        ptr[i] = malloc(seq_length * sizeof(double));
+        pi[i] = malloc(seq_length * sizeof(double));
         
         if( !vprob[i] || !ptr[i] || !pi[i] )
         {
-            printf("Not enough memory.");
+            fprintf(stderr, "Not enough memory.");
             return 1;
         }
     }
@@ -150,7 +169,7 @@ int main (int argc, char *argv[])
     double row1;
     
     // viterbi algorithm in log space to avoid underflow
-    for (int i = 1; i < n; i++)
+    for (int i = 1; i < seq_length; i++)
     {
         for (int j = 0; j < 2; j++)
         {
@@ -162,11 +181,11 @@ int main (int argc, char *argv[])
             pi[j][i] = max( row0 , row1 );
         }
     }
-    free(seq);
+    free(sequence);
     
     // traceback to find most likely path
-    path[n - 1] = argmax( pi[0][n - 1], pi[1][n - 1] );
-    for (int i = n - 2; i > 0; i--)
+    path[seq_length - 1] = argmax( pi[0][seq_length - 1], pi[1][seq_length - 1] );
+    for (int i = seq_length - 2; i > 0; i--)
     {
         path[i] = ptr[path[i + 1]][i + 1];
     }
@@ -184,7 +203,7 @@ int main (int argc, char *argv[])
 
     // print viterbi result
     printf("Viterbi output:\n");
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < seq_length; i++)
     {   
         if (i % 60 == 0 && i != 0)
         {
@@ -205,30 +224,23 @@ int main (int argc, char *argv[])
     return 0;
 }
 
-
-double max (double a, double b)
+int main (int argc, char *argv[]) 
 {
-    if (a > b)
+    // check for correct number of command line args
+    if (argc != 2 && argc != 3)
     {
-        return a;
-    }
-    else if (a < b)
-    {
-    return b;
-    }
-    // if equal, returns arbitrary argument for specific use in this algorithm
-    return b;
-}
-
-int argmax (double row0, double row1)
-{
-    if (row0 > row1)
-    {
-        return 0;
-    }
-    else if (row0 < row1)
-    {
+        fprintf(stderr, "Usage: ./viterbi my_sequence_file.txt [my_state_file.txt]. Include at least sequence file.\n");
         return 1;
     }
-    return 1;
+    
+    read_sequencefile(argv[1], &sequence_length);
+    
+    if(argv[2])
+    {
+        read_statefile(argv[2]);
+    }
+    
+    run_viterbi(sequence, sequence_length);
+
+    return 0;
 }
